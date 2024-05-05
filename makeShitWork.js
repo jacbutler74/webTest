d0d71732def95a3a0067774d75c528dd9b574e68
jacbutler74 <jacbutler74@gmail.com>
2024-05-04 21:31:49 -0700

    More Added

--- makeShitWork.js
+++ makeShitWork.js
@@ -21,24 +21,6 @@ function link() {
 }
 
 
-function vid() {
-    var video = document.getElementById('source_one');
-    var source = video.querySelector('source');
-    video.pause();
-
-    if (source.getAttribute('src') === "Introducing Cyn.mp4") {
-        video.src = "2nd.mp4";
-        source.src = "2nd.mp4";
-        video.play();
-    }
-    else {
-        video.src = "Introducing Cyn.mp4";
-        source.src = "Introducing Cyn.mp4";
-        video.play();
-
-    }
-}
-
 function vidImg(url) {
     document.getElementById('source_one').src = url;
     
